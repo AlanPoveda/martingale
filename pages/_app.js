b262a34a555629a65986c42baacfa2df9a770f3f
AlanPoveda <alsepoveda@gmail.com>
2021-03-29 22:27:24 -0300

    🎨 Colocando os novos componentes na página principal

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,20 @@
 import GithubCorner from '../src/components/GithubCorner'
 import HeaderP from '../src/components/HeaderP'
+import InputDados from '../src/components/Input'
+import ButtonLose from '../src/components/ButtonLose'
+import ButtonWin from '../src/components/ButtonWin'
 
 function MyApp() {
   return (
-    <>
+    <div>
       <HeaderP />
+      <InputDados />
+      <ButtonLose />
+      <ButtonWin />
+      <InputDados />
+
       <GithubCorner projectUrl="https://github.com/AlanPoveda" />
-    </>
+    </div>
   )
 }
 
