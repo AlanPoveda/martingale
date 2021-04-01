7bfd3c2932e561f125aa11509c24f5c355d78c5e
AlanPoveda <alsepoveda@gmail.com>
2021-03-31 21:14:59 -0300

    💄 Arrumando o css do Header

--- src/components/HeaderP/index.js
+++ src/components/HeaderP/index.js
@@ -1,7 +1,16 @@
+import styled from 'styled-components'
+
+const TitleHeader = styled.h1`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+`
+
 export default function HeaderP(){
     return(
         <>
-            <h1> Martingale </h1>
+            <TitleHeader> Martingale </TitleHeader>
         </>
     )
 }
