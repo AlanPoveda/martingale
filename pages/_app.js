68a48c4b0910d189510500a6ce9f920ecbf6199f
AlanPoveda <alsepoveda@gmail.com>
2021-03-31 21:15:55 -0300

    💄 Arrumando o lugar dos componentes

--- pages/_app.js
+++ pages/_app.js
@@ -1,21 +1,55 @@
-import GithubCorner from '../src/components/GithubCorner'
-import HeaderP from '../src/components/HeaderP'
-import InputDados from '../src/components/Input'
-import ButtonLose from '../src/components/ButtonLose'
-import ButtonWin from '../src/components/ButtonWin'
+import GithubCorner from "../src/components/GithubCorner";
+import styled from "styled-components";
+
+import HeaderP from "../src/components/HeaderP";
+
+import InputBanca from "../src/components/Inputs/InputBanca";
+import InputObjetive from "../src/components/Inputs/InputObjetive";
+import InputPayout from "../src/components/Inputs/InputPayout";
+import InputQtdEntradas from "../src/components/Inputs/InputQtdEntradas";
+
+import ValueMartingale from "../src/components/ValueMartingale";
+import ButtonLose from "../src/components/ButtonLose";
+import ButtonWin from "../src/components/ButtonWin";
+
+const Conteudo = styled.div`
+  display: flex;
+  justify-content: space-around;
+`;
+
+const Dados = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+const Values = styled.div`
+  display: flex;
+`;
+
+const Buttons = styled.div`
+  display: flex;
+`
 
 function MyApp() {
   return (
-    <div>
+    <>
       <HeaderP />
-      <InputDados />
-      <ButtonLose />
-      <ButtonWin />
-      <InputDados />
-
-      <GithubCorner projectUrl="https://github.com/AlanPoveda" />
-    </div>
-  )
+      <Conteudo>
+        <Dados>
+          <InputBanca />
+          <InputObjetive />
+          <InputPayout />
+          <InputQtdEntradas />
+        </Dados>
+        <Values>
+          <ValueMartingale />
+          <Buttons> 
+            <ButtonLose />
+            <ButtonWin />
+          </Buttons>
+        </Values>
+      </Conteudo>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
