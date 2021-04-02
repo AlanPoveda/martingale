fafa340ebc76cef99e054d5eb8e83b0625f3aa51
AlanPoveda <alsepoveda@gmail.com>
2021-04-02 19:02:13 -0300

    Upando site

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled, { createGlobalStyle } from "styled-components";
 
 import HeaderP from "../src/components/HeaderP";
 
@@ -11,17 +11,28 @@ import ValueMartingale from "../src/components/ValueMartingale";
 import ButtonLose from "../src/components/ButtonLose";
 import ButtonWin from "../src/components/ButtonWin";
 
+const Globalstyle = createGlobalStyle`
+   body {
+    margin: 0;
+    padding: 0;
+    display: flex;
+    flex-direction: column;
+    font-family: 'Lato', sans-serif;
+    color: #f5f5f5;
+
+  }
+`
+
+const BackgroundImage = styled.div`
+  background-image: url("https://s29755.pcdn.co/wp-content/uploads/2019/02/bullvsbear-1.jpg");
+  background-size: cover;
+  height: 100vh;
+  display: flex;
+`
+
 function MyApp() {
   return (
-    <div
-      style={{
-        backgroundImage: `url("https://s29755.pcdn.co/wp-content/uploads/2019/02/bullvsbear-1.jpg")
-      `,
-        backgroundSize: "cover",
-        height: "100vh",
-        color: "#f5f5f5",
-      }}
-    >
+    <BackgroundImage>
       <HeaderP />
 
       <InputBanca />
@@ -33,7 +44,8 @@ function MyApp() {
 
       <ButtonLose />
       <ButtonWin />
-    </div>
+      <Globalstyle />
+    </BackgroundImage>
   );
 }
 
